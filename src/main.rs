a09a7ab40e5d9998fb949c8733d4cd9e3a703d83
samdoesnerdstuff <206446103+samdoesnerdstuff@users.noreply.github.com>
2025-10-10 11:54:40 -0230

    (src): Removed cli, added help stuff to main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,49 @@
-mod cli;
+//! main.rs
+//! 
+//! This is the compiler entrypoint, it takes in user passed arguments
+//! before handing off to the lexer, which will search for the source
+//! code and start the compilation process.
+//! 
+//! 
 
-use anyhow::Result;
+use std::env;
 
-fn main() -> Result<()> {
-    cli::run();
+fn main() {
+    let cli_args: Vec<String> = env::args().collect();
+    
+    for arg in cli_args.iter().skip(1) {
+        match arg.as_str() {
+            "-help" => display_help(),
+            "-version" => display_version(),
+            "-help-compiler-flags" => all_compiler_flags(),
+            &_ => println!("")
+        }
+    }
+}
+
+fn display_help() {
+    println!("* Lambda Compiler");
+    println!("* Version {}", env!("CARGO_PKG_VERSION"));
+    println!("* Target Arch: {}", std::env::consts::ARCH);
+    println!("* Target OS: {}", std::env::consts::OS);
+    println!("* Usage:");
+    println!("*    -help    => Displays help menu");
+    println!("*    -version => Displays compiler version");
+    println!("*");
+    println!("* Compiler Flags:");
+    println!("*    -s => Specify source file(s) for compilation");
+    println!("*    -o => Specify output location (defaults to wherever lamc is invoked)");
+    println!("*    -wl (low,med,hi) => Compiler warning level");
+    println!("*    -opt (0-3) => Compiler optimization level");
+    println!("*");
+    println!("* For ALL compilation flags, use '-help-compiler-flags' ");
+    println!("* (Recommended that you pipe the output to a text file)");
+}
+
+fn all_compiler_flags() {
+
+}
+
+fn display_version() {
+    println!("{}", env!("CARGO_PKG_VERSION"));
 }
